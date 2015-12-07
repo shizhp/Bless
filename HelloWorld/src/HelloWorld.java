011f115478cbe39657de8ebee6b4ab1e51026b39
shizhp <shizhp@succez.com>
2015-12-07 15:13:39 +0800

    commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,11 @@
+//github push test
+//shizhp
+//15-12-7
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");// TODO Auto-generated method stub
+
+	}
+
+}
