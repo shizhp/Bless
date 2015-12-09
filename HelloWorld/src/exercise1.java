960b73c534128a5a0104ae98a08afc2c3ef402cb
shizhp <shizhp@succez.com>
2015-12-09 11:40:39 +0800

    file2byte v1.1 add finally use inputstream and ByteArrayOutputStream

--- HelloWorld/src/exercise1.java
+++ HelloWorld/src/exercise1.java
@@ -1,4 +1,9 @@
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.File;
 public class exercise1 {
 	/*turn a file into a byte buffer
 	 * if the file doesn't exit or read error return null 
@@ -6,54 +11,82 @@ public class exercise1 {
 	 */
 	public static byte[] file2buf(File fobj)
 	{
+		byte[] buff= null;//返回字节数组
+		InputStream fis = null;//定义文件读取输入流
+		ByteArrayOutputStream baos = null;//定义文件输出流
 		try{
-			byte[] buff=null;
+			
 			if(fobj.isFile()&&fobj.exists())
 			{	//return buff;
-				int buffLength=(int)fobj.length();//根据文件的长度来分配byte数组长度
-				buff=new byte[buffLength];
-				FileInputStream fis=new FileInputStream(fobj);
-				fis.read(buff);
-				fis.close();
-				return buff;
-			}
-			else
-			{
-				System.out.println("error in reading file");
-				return buff;
+				//int buffLength=(int)fobj.length();//根据文件的长度来分配byte数组长度
+				byte[] tmpBuff = new byte[4096];//缓冲字节数组
+				fis =new FileInputStream(fobj);
+				baos = new ByteArrayOutputStream(4096);
+				int tmpbyte = 0;//获取文件读取信息
+				while((tmpbyte = fis.read(tmpBuff)) != -1)
+				{
+					baos.write(tmpBuff,0,tmpbyte);
+				}
+				//fis.read(buff);
+				//fis.close();
+				buff = baos.toByteArray();
+				// outByte = fis; 
+				
 			}
 		}
-		catch(Exception e)
+		catch(FileNotFoundException e)
 		{
-			byte[] buff=null;
-			return buff;
-			//System.out.println("error");
-			//e.printStackTrace();
+			e.printStackTrace();
 		}
+		catch(IOException e)
+		{			
+			e.printStackTrace();
+		}
+		finally
+		{
+			if(fis != null)
+			{
+				try {
+					fis.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			if(baos != null)
+			{
+				try
+				{
+					baos.close();
+				}
+				catch(IOException e)
+				{	
+				}
+			}
+		}
+		return buff;
 	}
 	/**
 	 * @param args
 	 * @throws Exception
 	 */
-	public static void main(String[] args) {//throws Exception{
+	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
-		File fobj=new File("D:\\test1.txt");
+		File fobj = null;
+
+		fobj=new File("D:/test.txt");	
 		byte[] buff=null;
 		//if(fobj.isFile()&&fobj.exists())//若文件存在 进行读取操作
 			buff=file2buf(fobj);
 		if(buff!=null)
 		{
-			System.out.println(buff.length);
+			System.out.println("数组长度为"+buff.length);
 			for(int i=0;buff!=null&&i<buff.length;i++)
 			{
 				System.out.println(buff[i]);
 			}
 		}
-		else
-			System.out.println("no such file exists");
-		//fobj.close();
-		
-		
+		//fobj.close();		
 	    }
 }
 
